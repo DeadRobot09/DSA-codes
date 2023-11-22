77cb2db2694899e6a59bd68336ff14a5cc13298f
DeadRobot09 <119564822+DeadRobot09@users.noreply.github.com>
2023-11-22 21:22:44 +0530

    Create jumpgame.cpp

---
+++ jumpgame.cpp
@@ -0,0 +1,17 @@
+/*You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.*/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        int reachable = 0;
+
+        for(int i=0 ;i<n; i++){
+            if(i>reachable) return false;
+            reachable = max(reachable , i+nums[i]);
+        }
+        return true;
+    }
+};
